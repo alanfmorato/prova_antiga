114a0f33895051bcaed17e078d1da353c993789c
Alan Morato <alanf.morato@gmail.com>
2023-10-02 22:40:56 +0000

    wip

--- src/main/java/br/gov/sp/fatec/repository/TrabalhoRepository.java
+++ src/main/java/br/gov/sp/fatec/repository/TrabalhoRepository.java
@@ -11,5 +11,5 @@ import br.gov.sp.fatec.entity.Trabalho;
 public interface TrabalhoRepository extends JpaRepository<Trabalho, Long>{
      
     @Query("select t from trabalho t where t.nota > :nota and lower(t.titulo) like %:palavra%")
-    List<Trabalho> findTrabalhosByNotaAndTituloContaining(Integer nota, String palavra)
+    List<Trabalho> findTrabalhosByNotaAndTituloContaining(Integer nota, String palavra);
 }
--- src/main/java/br/gov/sp/fatec/service/TrabalhoService.java
+++ src/main/java/br/gov/sp/fatec/service/TrabalhoService.java
@@ -1,29 +1,47 @@
 package br.gov.sp.fatec.service;
 
 import java.util.List;
+import java.util.Optional;
 
-import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import br.gov.sp.fatec.entity.Trabalho;
+import br.gov.sp.fatec.repository.TrabalhoRepository;
 
 public class TrabalhoService implements ITrabalhoService {
+    @Autowired
+    private TrabalhoRepository traRepo;
 
     @Override
     public Trabalho buscaPorId(Long id) {
+        Optional<Trabalho> trabalhoOP = traRepo.findById(id);
+        if(trabalhoOP.isPresent()){
+            return trabalhoOP.get();
+
+        }
         // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'buscaPorId'");
+        throw new UnsupportedOperationException("id Invalido");
     }
 
     @Override
     public Trabalho novoTrabalho(Trabalho trabalho) {
+        if(trabalho == null ||
+        trabalho.getId() == null ||
+        trabalho.getTitulo() == null ||
+        trabalho.getNota() == null ||
+        trabalho.getJustificativa() == null ||
+        trabalho.getDescricao() == null ||
+        trabalho.getData_hora_entrega() == null){
+        throw new UnsupportedOperationException("Valores Inválidos");
+        }
+        return traRepo.save(trabalho);
         // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'novoTrabalho'");
     }
 
     @Override
     public List<Trabalho> buscarTodos() {
+        return traRepo.findAll();
         // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'buscarTodos'");
     }
     
 }
